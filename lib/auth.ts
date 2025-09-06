3988035983673e2c6986b4a8dc428833cfb1c98c
CleeSmith <cleesmith2006@gmail.com>
2025-09-06 16:41:03 -0400

    privacy/terms and lesser scopes on google Drive

--- lib/auth.ts
+++ lib/auth.ts
@@ -52,7 +52,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          scope: 'openid email profile https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/documents',
+          // scope: 'openid email profile https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/documents',
+          scope: 'openid email profile https://www.googleapis.com/auth/drive.file',
           prompt: 'consent',
           access_type: 'offline',
           response_type: 'code'
